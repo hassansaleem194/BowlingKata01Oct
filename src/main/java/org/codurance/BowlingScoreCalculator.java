60741bd654e80d8abfb09ffeb15b1cb2d3ebfb02
Hassan <hassansaleem194@gmail.com>
2022-10-01 22:55:57 +0100

    Refactored score calculator which includes strikes

--- src/main/java/org/codurance/BowlingScoreCalculator.java
+++ src/main/java/org/codurance/BowlingScoreCalculator.java
@@ -3,35 +3,60 @@ package org.codurance;
 import java.util.Objects;
 
 public class BowlingScoreCalculator {
+
+    public static final String STRIKE = "X";
+    public static final int STRIKE_POINTS = 10;
+
     public int score(String scoreCard) {
-
-
-//        split("|")
-//        will return array
-//            array function first last
-//                if first = "-" add 0
-//            else convert string to integer
-//                atomicinteger create running total / return score
-
-
         String[] scoreArray = scoreCard.split("\\|");
+        return calculateScore(scoreArray);
+    }
 
+    private int calculateScore(String[] scoreArray) {
         int score = 0;
-
         for (int i = 0; i < scoreArray.length; i++) {
-            String[] frame = scoreArray[i].split("");
+            String frame = scoreArray[i];
+            String[] frameArray = frame.split("");
+            score += parseScore(frameArray[0]);
 
-            score += parseScore(frame[0]);
-            score += parseScore(frame[1]);
+            if (!isAStrike(frameArray)) {
+                score += addSecondBowlOfFrame(frameArray);
 
+            } else if (isNextFrameAStrike(scoreArray, i)) {
+                score += STRIKE_POINTS;
+                String scoredTwoStrikesConsecutively = scoreArray[i + 2].split("")[0];
+                score += parseScore(scoredTwoStrikesConsecutively);
+
+            } else {
+                String[] nextFrame = scoreArray[i + 1].split("");
+                score += parseScore(nextFrame[0]);
+                score += parseScore(nextFrame[1]);
+            }
         }
-            return score;
+        return score;
+    }
+
+    private boolean isNextFrameAStrike(String[] scoreArray, int i) {
+        return scoreArray[i + 1].equals(STRIKE);
+    }
+
+    private int addSecondBowlOfFrame(String[] frameArray) {
+        return parseScore(frameArray[1]);
+    }
+
+    private boolean isAStrike(String[] frameArray) {
+        return frameArray[0].equals(STRIKE);
     }
 
     private int parseScore(String attempt) {
+        if (Objects.equals(attempt, STRIKE)) {
+            return STRIKE_POINTS;
+        }
+
         if (!Objects.equals(attempt, "-")) {
             return Integer.parseInt(attempt);
         }
         return 0;
     }
 }
+
--- src/test/java/BowlingScoreFeatureTest.java
+++ src/test/java/BowlingScoreFeatureTest.java
@@ -1,4 +1,6 @@
 import org.codurance.BowlingScoreCalculator;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
@@ -11,13 +13,48 @@ import static org.junit.jupiter.params.provider.Arguments.arguments;
 public class BowlingScoreFeatureTest {
 
 
+    private final BowlingScoreCalculator underTest = new BowlingScoreCalculator();
+
     @ParameterizedTest
-    @MethodSource("scoreCards")
-    public void should_return_a_completed_game_with_0_points(String scoreCard, int finalScore) {
-        assertEquals(finalScore, new BowlingScoreCalculator().score(scoreCard));
+    @MethodSource("singlePointScoreCards")
+    public void should_return_a_completed_game_with_point_on_first_bowl(String scoreCard, int finalScore) {
+        assertEquals(finalScore, underTest.score(scoreCard));
     }
 
-    private static Stream<Arguments> scoreCards(){
+    @Test
+    public void should_return_a_completed_game_with_two_scores_in_the_first_frame() {
+        assertEquals(9, underTest.score("36|--|--|--|--|--|--|--|--|--||"));
+    }
+
+    @Test
+    public void should_return_a_completed_game_with_multiple_scores_in_the_whole_game() {
+        assertEquals(42, underTest.score("36|--|4-|6-|-6|--|3-|6-|8-|--||"));
+
+    }
+
+    @Test
+    public void should_return_10_when_you_score_a_strike() {
+        assertEquals(10, underTest.score("X|--|--|--|--|--|--|--|--|--||"));
+    }
+
+    @Test
+    public void should_return_correct_score_when_you_score_a_strike_and_non_strikes() {
+        assertEquals(17, underTest.score("X|--|3-|4-|--|--|--|--|--|--||"));
+    }
+
+    @Test
+    public void should_return_correct_score_when_you_score_two_strikes_and_non_strike_consecutively() {
+        assertEquals(33, underTest.score("X|X|1-|--|--|--|--|--|--|--||"));
+    }
+
+    @Test
+    public void should_return_correct_score_when_you_score_a_strike_and_non_strike_consecutively() {
+        assertEquals(20, underTest.score("X|23|--|--|--|--|--|--|--|--||"));
+    }
+
+
+
+    private static Stream<Arguments> singlePointScoreCards() {
         return Stream.of(
                 arguments("--|--|--|--|--|--|--|--|--|--||", 0),
                 arguments("1-|--|--|--|--|--|--|--|--|--||", 1),
